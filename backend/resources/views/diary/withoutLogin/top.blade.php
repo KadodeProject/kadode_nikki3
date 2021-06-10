74f896ac0c7fe3427c40ee4c1a9d9af91fa76307
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-06-11 08:57:41 +0900

    feat 各種エラーページ整備

---
+++ backend/resources/views/diary/withoutLogin/top.blade.php
@@ -0,0 +1,15 @@
+
+@extends("layouts.main")
+@section("title","トップ")
+
+@section('header')
+@parent
+@endsection
+@section('content')
+<div class="board-main">
+   
+    
+</div>
+      
+@endsection
+ 
---
+++ backend/resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+
+@extends("layouts.error")
+@section("title","404")
+
+@section('header')
+@parent
+@endsection
+@section('content')
+<div class="board-main">
+   
+    <h1 class="text-center text-3xl my-8 kiwi-maru">404 エラーです。このページは存在しません。</h1>
+    <img src="img/errorPages/shigureni404.png" class="mx-auto object-contain h-80 w-80">
+</div>
+      
+@endsection
+ 
