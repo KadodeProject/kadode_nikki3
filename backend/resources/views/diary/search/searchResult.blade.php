e71794e5a73f56fa0c0f89636798ada52588ed4b
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-06-13 22:22:18 +0900

    feat 検索ページの検索窓

--- backend/resources/views/diary/search/searchResult.blade.php
+++ backend/resources/views/diary/search/searchResult.blade.php
@@ -7,22 +7,53 @@
 @endsection
 @section('content')
 <div class="board-main">
+
+
 @empty($keyword)
 <h1 class="text-center text-3xl my-4">検索ページ</h1>
 <div style="min-height:100vh">
-    <form class="move-label-wrapper flex"method="POST" action="/search">
-        @csrf
-        <p class="text-xl">キーワード検索</p>
-        <div>
-            <input  autocomplete="off"class="search-keyword" type="search" name="keyword" placeholder="キーワード(2~20字)">
-        </div>
-        <input type="submit" value="検索">
-        
-    </form>
+    @if(count($errors)>0)
+    {{-- エラーの表示 --}}
+    <ul class="text-red-500 text-center">
+        @foreach($errors->all() as $error)
+        <li>{{$error}}</li>
+        @endforeach
+    </ul>
+    @endif
+    <div class="search-form my-12 flex justify-center">
+        <form class="move-label-wrapper flex items-center "method="POST" action="/search">
+            @csrf
+            <p class="text-xl">キーワード検索</p>
+            <div>
+                <input  autocomplete="off"class="search-keyword" type="search" name="keyword"  placeholder="キーワード(2~20字)">
+            </div>
+            <input type="submit" class="h-full"value="検索">
+            
+        </form>
+    </div>
 </div>
 @else
-    
 <h1 class="text-center text-3xl my-4">「{{$keyword}}」の検索結果 <span class="text-sm">[{{$counter}}件(最大200件)]</span><span class="text-xs">[クエリ時間:{{$queryTime}}ミリ秒]</span></h1>
+    @if(count($errors)>0)
+    {{-- エラーの表示 --}}
+    <ul class="text-red-500 text-center">
+        @foreach($errors->all() as $error)
+        <li>{{$error}}</li>
+        @endforeach
+    </ul>
+    @endif
+<div class="search-form my-12 flex justify-center">
+    <form class="move-label-wrapper flex items-center "method="POST" action="/search">
+        @csrf
+    <p class="text-xl">キーワード検索</p>
+    <div>
+        <input  autocomplete="off"class="search-keyword" type="search" name="keyword" value="{{$keyword}}" placeholder="キーワード(2~20字)">
+    </div>
+    <input type="submit" class="h-full"value="検索">
+        
+</form>
+</div>
+
 <div class="flex w-full m-4 justify-center flex-wrap" >
     @empty($diaries)
         <h3 class="text-center text-3xl my-20">「{{$keyword}}」を含む日記はありません！</h3>
---
+++ backend/resources/views/errors/429.blade.php
@@ -0,0 +1,15 @@
+@extends("layouts.noLogIn")
+@section("title","429")
+
+@section('header')
+@parent
+@endsection
+@section('content')
+<div class="board-main error-page">
+   
+    <h1 class="text-center text-3xl my-8 kiwi-maru">429 エラーです。大量のリクエストがありました。</h1>
+    <img src="img/errorPages/shigureni429.png" class="mx-auto object-contain h-80 w-80">
+</div>
+      
+@endsection
+ 
