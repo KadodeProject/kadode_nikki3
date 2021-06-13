7099f9977439a3b16ac091bfcad4c1862eb5682f
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-06-14 08:22:45 +0900

    fix 微調整(サーバーデプロイ用)

--- backend/resources/views/layouts/noLogIn.blade.php
+++ backend/resources/views/layouts/noLogIn.blade.php
@@ -19,8 +19,8 @@
     {{-- OGP --}}
     <meta property="og:title" content="@yield('title') | かどで日記">
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://kadode_nikki3.usuyuki.net/">
-    <meta property="og:image" content="https://kadode_nikki3.usuyuki.net/img/ogp.png">
+    <meta property="og:url" content="https://kadodenikki3.usuyuki.net/">
+    <meta property="og:image" content="https://kadodenikki3.usuyuki.net/img/ogp.png">
     <meta property="og:site_name" content="かどで日記">
     <meta property="og:description" content="かどで日記は日記を管理できるwebアプリです">
     <meta name="twitter:card" content="summary">
