efa9c2875309ef858c2003c8b01b7a7cdfe70b1d
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-07-03 13:38:39 +0900

    wip 緊急解除

--- backend/app/Http/Controllers/statistics/updateStatisticsController.php
+++ backend/app/Http/Controllers/statistics/updateStatisticsController.php
@@ -21,7 +21,7 @@ class updateStatisticsController extends Controller
         // \Log::debug($yesterday->diffInHours($static->updated_at));
         
         // 24時間以内なら更新しない
-        if(($yesterday->diffInHours($static->updated_at))>=24){
+        if(($yesterday->diffInHours($static->updated_at))>=0){
             $diaries=Diary::orderby("date","asc")->get();
 
             $calculateDiary=calculateDiary::calculateDiary($diaries);
