df7943a166bc76927a533c853e11628a1234bdca
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-09-05 11:13:05 +0900

    wip

---
+++ backend/app/CustomFunction/throwPython.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\CustomFunction;
+
+
+
+
+
+class throwPython
+{
+    public function access_py($py_file_name,$user_id,$error_check=false){
+  
+    }
+    public static function throwPython($py_file_name,$user_id,$error_check=false,$debug=false){
+        if($error_check==true){
+            //2>&1でエラーメッセージ出せる
+            $error_check="2>&1";
+        }else{
+            $error_check="";
+        }
+  
+
+        $path = "export LANG=ja_JP.UTF-8;python3"." ".env('PYTHON_FOLDER_DIR')."/". $py_file_name.".py". " ".$user_id." ".$error_check;
+        $output=null;
+        exec($path, $output);//python実行
+
+        if($debug==true){
+            \Log::debug("file_name:".$py_file_name);
+            \Log::debug("python_output");
+            \Log::debug($output);
+        }
+
+        return $output;
+    }
+}
