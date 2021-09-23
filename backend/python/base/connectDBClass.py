e3191a5ea739b938f954002c0ac5065f76157237
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-09-23 22:00:42 +0900

    wip classの最後の希望「

--- backend/python/base/connectDBClass.py
+++ backend/python/base/connectDBClass.py
@@ -12,7 +12,7 @@ class connectDB:
     )
     
     def __init__(self):
-
+        self.conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
         print('DB接続処理')
     def __del__(self):
         self.conn.close()
@@ -33,6 +33,7 @@ class connectDB:
         rows = cur.fetchall()
         # カーソルを閉じる
         cur.close()
+        self.conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
         return rows
     """
     データベースに接続して、引数のユーザーIDのすべての日記の自然言語Pre情報を取得
@@ -49,6 +50,7 @@ class connectDB:
         rows = cur.fetchall()
         # カーソルを閉じる
         cur.close()
+        self.conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
         return rows
 
     """
@@ -64,6 +66,7 @@ class connectDB:
         self.conn.commit()
         # カーソルを閉じる
         cur.close()
+        self.conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
     
     """
     解析済みのノーマルデータを書き込む(1ユーザー複数テーブルのもの用)
@@ -79,6 +82,7 @@ class connectDB:
             self.conn.commit()
             # カーソルを閉じる
             cur.close()
+            self.conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
 
     """
     解析済みのJSONデータを書き込む(1ユーザー複数テーブルのもの用)
@@ -95,6 +99,7 @@ class connectDB:
             self.conn.commit()
             # カーソルを閉じる
             cur.close()
+            self.conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
 
 
 
@@ -112,6 +117,7 @@ class connectDB:
         self.conn.commit()
         # カーソルを閉じる
         cur.close()
+        
 
     """
     進捗状況--統計テーブルなど1ユーザー1テーブルのもの用
@@ -127,6 +133,185 @@ class connectDB:
         self.conn.commit()
         # カーソルを閉じる
         cur.close()
+# """
+# データベースに接続して、引数のユーザーIDのすべての日記の基本情報を取得
+# """
+# def get_all_diaries_from_user(user_id):
+#     conn = MySQLdb.connect(
+#     user=loadEnv.DB_USERNAME,
+#     passwd=loadEnv.DB_PASSWORD,
+#     host=loadEnv.DB_HOST,
+#     db=loadEnv.DB_DATABASE,
+#     charset="utf8"
+#     )
+#     conn.ping(True)
+#     print('日記の取得')
+#     # カーソルを取得する
+#     cur= conn.cursor()
+#     # クエリを実行する
+#     #このクエリの順番は他所でrow[2]的な依存をしているので変更は要注意
+#     sql = "SELECT id,title,content,date,updated_at,updated_statistic_at FROM diaries WHERE user_id="+str(user_id)+";"
+#     cur.execute(sql)
+#     # 実行結果をすべて取得する
+#     rows = cur.fetchall()
+#     # カーソルを閉じる
+#     cur.close()
+#     conn.close()
+#     # conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
+#     return rows
+# """
+# データベースに接続して、引数のユーザーIDのすべての日記の自然言語Pre情報を取得
+# """
+# def get_all_diariesPre_from_user(user_id):
+#     conn = MySQLdb.connect(
+#     user=loadEnv.DB_USERNAME,
+#     passwd=loadEnv.DB_PASSWORD,
+#     host=loadEnv.DB_HOST,
+#     db=loadEnv.DB_DATABASE,
+#     charset="utf8"
+#     )
+#     conn.ping(True)
+#     print('日記の取得')
+#     # カーソルを取得する
+#     cur= conn.cursor()
+#     # クエリを実行する
+#     #このクエリの順番は他所でrow[2]的な依存をしているので変更は要注意
+#     sql = "SELECT id,updated_at,updated_statistic_at,sentence,chunk,token,affiliation,char_length,content FROM diaries WHERE user_id="+str(user_id)+";"
+#     cur.execute(sql)
+#     # 実行結果をすべて取得する
+#     rows = cur.fetchall()
+#     # カーソルを閉じる
+#     cur.close()
+#     conn.close()
+#     # conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
+#     return rows
+
+# """
+# 解析済みのJSONデータを書き込む
+# """
+# def set_statistics_json(user_id, column_name, value):
+#     conn = MySQLdb.connect(
+#     user=loadEnv.DB_USERNAME,
+#     passwd=loadEnv.DB_PASSWORD,
+#     host=loadEnv.DB_HOST,
+#     db=loadEnv.DB_DATABASE,
+#     charset="utf8"
+#     )
+#     conn.ping(True)
+#     # カーソルを取得する
+#     cur= conn.cursor()        # クエリを実行する ここはsqkインジェクションにならないところなので、そのまま自家絵やり倒す
+#     json_value = json.dumps(value,ensure_ascii=False)
+#     cur.execute(
+#             'UPDATE statistics SET {0} = %s WHERE user_id = %s;'.format(column_name),(json_value,user_id))
+#     # 保存する
+#     conn.commit()
+#     # カーソルを閉じる
+#     cur.close()
+#     conn.close()
+#     # conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
+
+# """
+# 解析済みのノーマルデータを書き込む(1ユーザー複数テーブルのもの用)
+# """
+# def set_single_normal_data(column,db_id,**values):
+#     conn = MySQLdb.connect(
+#     user=loadEnv.DB_USERNAME,
+#     passwd=loadEnv.DB_PASSWORD,
+#     host=loadEnv.DB_HOST,
+#     db=loadEnv.DB_DATABASE,
+#     charset="utf8"
+#     )
+#     conn.ping(True)
+#     for (key,value) in values.items():
+#         # カーソルを取得する
+#         cur= conn.cursor()
+#         # クエリを実行する ここはsqkインジェクションにならないところなので、そのまま
+#         cur.execute(
+#                 'UPDATE {0} SET {1} = %s WHERE id = %s;'.format(column,key),(value,db_id))
+#         # 保存する
+#         conn.commit()
+#         # カーソルを閉じる
+#         cur.close()
+#         conn.close()
+#         # conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
+
+# """
+# 解析済みのJSONデータを書き込む(1ユーザー複数テーブルのもの用)
+# """
+# def set_single_json_data(column,db_id,**jsons):
+#     conn = MySQLdb.connect(
+#     user=loadEnv.DB_USERNAME,
+#     passwd=loadEnv.DB_PASSWORD,
+#     host=loadEnv.DB_HOST,
+#     db=loadEnv.DB_DATABASE,
+#     charset="utf8"
+#     )
+#     conn.ping(True)
+#     for (key,value) in jsons.items():
+#         # カーソルを取得する
+#         cur= conn.cursor()
+#         # クエリを実行する ここはsqkインジェクションにならないところなので、そのまま
+#         json_value = json.dumps(value,ensure_ascii=False)
+#         cur.execute(
+#                 'UPDATE {0} SET {1} = %s WHERE id = %s;'.format(column,key),(json_value,db_id))
+#         # 保存する
+#         conn.commit()
+#         # カーソルを閉じる
+#         cur.close()
+#         conn.close()
+#         # conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
+
+
+
+# """
+# 進捗状況--日記テーブルなど1ユーザー複数テーブルのもの用
+# """
+# def set_single_progress(db_id,table_name,value):
+#     conn = MySQLdb.connect(
+#     user=loadEnv.DB_USERNAME,
+#     passwd=loadEnv.DB_PASSWORD,
+#     host=loadEnv.DB_HOST,
+#     db=loadEnv.DB_DATABASE,
+#     charset="utf8"
+#     )
+#     conn.ping(True)
+#     # カーソルを取得する
+#     cur= conn.cursor()
+#     # クエリを実行する
+#     cur.execute(
+#             'UPDATE {0} SET statistic_progress = %s where id = %s;'.format(table_name),(str(value),str(db_id))
+#             )
+#     # 保存する
+#     conn.commit()
+#     # カーソルを閉じる
+#     cur.close()
+#     conn.close()
+    
+
+# """
+# 進捗状況--統計テーブルなど1ユーザー1テーブルのもの用
+# """
+# def set_multiple_progress(user_id, table_name,value):
+#     conn = MySQLdb.connect(
+#     user=loadEnv.DB_USERNAME,
+#     passwd=loadEnv.DB_PASSWORD,
+#     host=loadEnv.DB_HOST,
+#     db=loadEnv.DB_DATABASE,
+#     charset="utf8"
+#     )
+#     conn.ping(True)
+#     # カーソルを取得する
+#     cur= conn.cursor()
+#     # クエリを実行する
+#     cur.execute(
+#             'UPDATE {0} SET statistic_progress = %s where user_id = %s;'.format(table_name),(str(value),str(user_id))
+#             )
+#     # 保存する
+#     conn.commit()
+#     # カーソルを閉じる
+#     cur.close()
+#     conn.close()
+
 
 
 
