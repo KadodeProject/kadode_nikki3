c4035d62d2a9c318ab1f6a2a3713bcc3efa4c67d
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-09-30 20:05:47 +0900

    wip sqlの初期動作

--- backend/python/base/connectDBClass.py
+++ backend/python/base/connectDBClass.py
@@ -131,14 +131,22 @@ class connectDB:
             self.conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
     """
     解析済みのノーマルデータを書き込む(user_idと年月で決まり、存在しない場合もあるもの用)
+    dateは配列で date[year,month]
     """
     def set_depDate_normal_data(self,column,user_id,date,**values):
         for (key,value) in values.items():
             # カーソルを取得する
             cur= self.conn.cursor()
             # クエリを実行する ここはsqkインジェクションにならないところなので、そのまま
-            cur.execute(
-                    'UPDATE {0} SET {1} = %s WHERE user_id = %s;'.format(column,key),(value,user_id))
+            if(column=="statistic_per_months"):
+                #年付きでwhereする
+                cur.execute(
+                        'UPDATE {0} SET {1} = %s WHERE user_id = %s AND year = %s AND month = %s;'.format(column,key),(value,user_id,date[0],date[1]))
+            else:
+                #年でwhereする
+                cur.execute(
+                        'UPDATE {0} SET {1} = %s WHERE user_id = %s AND year = %s;'.format(column,key),(value,user_id,date[0]))
+
             # 保存する
             self.conn.commit()
             # カーソルを閉じる
@@ -147,6 +155,7 @@ class connectDB:
 
     """
     解析済みのJSONデータを書き込む(user_idと年月で決まり、存在しない場合もあるもの用)
+    dateは配列で date[year,month]
     """
     def set_depDate_json_data(self,column,user_id,date,**jsons):
         for (key,value) in jsons.items():
@@ -154,9 +163,13 @@ class connectDB:
             cur= self.conn.cursor()
             # クエリを実行する ここはsqkインジェクションにならないところなので、そのまま
             json_value = json.dumps(value,ensure_ascii=False)
-            cur.execute(
-                    'UPDATE {0} SET {1} = %s WHERE user_id = %s;'.format(column,key),(json_value,user_id))
-            # 保存する
+            if(column=="statistic_per_months"):
+                cur.execute(
+                        'UPDATE {0} SET {1} = %s WHERE user_id = %s AND year = %s AND month = %s;'.format(column,key),(json_value,user_id,date[0],date[1]))
+            else:
+                cur.execute(
+                        'UPDATE {0} SET {1} = %s WHERE user_id = %s AND year = %s;'.format(column,key),(json_value,user_id,date[0]))
+
             self.conn.commit()
             # カーソルを閉じる
             cur.close()
@@ -201,13 +214,44 @@ class connectDB:
         # カーソルを取得する
         cur= self.conn.cursor()
         # クエリを実行する
-        cur.execute(
-                'UPDATE {0} SET statistic_progress = %s where user_id = %s;'.format(table_name),(str(value),str(user_id))
-                )
+        if(table_name=="statistic_per_months"):
+            cur.execute(
+                    'UPDATE {0} SET statistic_progress = %s where user_id = %s AND year = %s AND month = %s;'.format(table_name),(str(value),str(user_id),date[0],date[1])
+                    )
+        else:
+            cur.execute(
+                    'UPDATE {0} SET statistic_progress = %s where user_id = %s AND year = %s ;'.format(table_name),(str(value),str(user_id),date[0])
+                    )
         # 保存する
         self.conn.commit()
         # カーソルを閉じる
         cur.close()
+
+    """
+    解析済みのノーマルデータを書き込む(user_idと年月で決まり、存在しない場合もあるもの用)
+    dateは配列で date[year,month]
+    """
+    def set_depDate_insertUpdate_data(self,column,user_id,date):
+        # カーソルを取得する
+        cur= self.conn.cursor()
+        # クエリを実行する ここはsqkインジェクションにならないところなので、そのまま
+        if(column=="statistic_per_months"):
+            #無ければ新しい列を作る
+
+            print(
+                    'SELECT * CASE NOT EXISTS(SELECT * FROM {0} WHERE user_id = {1} AND year = {2} AND month = {3}) THEN INSERT {0} INTO (user_id,year,month,statistic_progress) VALUES ({1},{2},{3},1) END;'.format(column,user_id,date[0],date[1]))
+            cur.execute(
+                    'CASE NOT EXISTS(SELECT * FROM {0} WHERE user_id = {1} AND year = {2} AND month = {3}) THEN INSERT {0} INTO (user_id,year,month,statistic_progress) VALUES ({1},{2},{3},1) END;'.format(column,user_id,date[0],date[1]))
+        else:
+            #年でwhereする
+            cur.execute(
+                    'IF EXISTS(SELECT * FROM {0} WHERE user_id = {1} AND year = {2} ) UPDATE {0} SET year = {2}  ELSE {0} INSERT INTO (user_id,year,statistic_progress) VALUES ({1},{2},1);'.format(column,user_id,date[0]))
+
+        # 保存する
+        self.conn.commit()
+        # カーソルを閉じる
+        cur.close()
+        self.conn.ping(True)#mysql2003エラー(サーバー接続切れ防止)
 # """
 # データベースに接続して、引数のユーザーIDのすべての日記の基本情報を取得
 # """
