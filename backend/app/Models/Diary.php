3b82bf0bebaa9cf4ea105407fcb0308f1131f2fa
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-12-07 13:50:29 +0900

    fix バリデーションが型の最大値以上になっていて、潜在的なリスクがあったものを修正

--- backend/app/Models/Diary.php
+++ backend/app/Models/Diary.php
@@ -35,22 +35,22 @@ class Diary extends Model
      * unsignedBigIntegerはlaravelのid標準
      * textタイプはmax 65,535バイトでマルチバイトなので大目に見て20000字
      * longTextというmax4,294,967,295バイトまで行けるものもある。
-     * 
+     *
      * numericが整数値のバリデーション
      * user_idは後で入れるので不要
-     * 
+     *
      */
     public static $rules=array(
         "date"=>"required",
-        "title"=>"max:50",
-        "content"=>"required|min:1|max:20000",
+        "title"=>"max:50",//laravelのstringはvarchar(255)なので、255文字まで、しかし入らないから50字に抑える
+        "content"=>"required|min:1|max:16000",//text型の限界が16384文字なので(マルチバイトで)
         // "user_id"=>"required|numeric",
         );
-    
+
     protected $fillable = [
             "statistic_progress","user_id","uuid","title","content","date" ,"sentence","chunk","token","affiliation","meta_info","similar_sentences","char_length","emotions","flavor","classification","important_words","cause_effect_sentences","special_people","updated_statistic_at","created_at","updated_at"
     ];
-    
+
      // 初期値設定(statistic_progressを0にする)
     protected $attributes = [
         "statistic_progress" =>0,
