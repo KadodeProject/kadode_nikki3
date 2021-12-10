fcd2a60c254b4da78bcb8de36538fc94633adc0c
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2021-12-10 14:21:09 +0900

    faet DBテーブル周りのマイグレーションとモデル作成

---
+++ backend/database/migrations/2021_12_08_130114_create_user_ranks_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserRanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_ranks', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->comment("ユーザーランク名");
+            $table->string("description")->nullable()->comment("説明");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_ranks');
+    }
+}
