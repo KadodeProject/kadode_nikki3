84497e41b9668ff1fcfe625a12761c7b248b60bf
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-01-21 18:18:12 +0900

    feat トップページのデザイン更新

---
+++ backend/resources/views/components/noLogIn/introFrame.blade.php
@@ -0,0 +1,9 @@
+<div class="my-12">
+    <h3 class="text-center my-4 kiwi-maru text-4xl"><span class="material-icons">{{$icon}}</span>{{$title}}</h3>
+    {{ $slot }}
+</div>
+<div class="flex items-center justify-center " style="height:300px">
+    <p class="material-icons text-3xl text-main-color">
+        arrow_downward
+    </p>
+</div>
--- backend/resources/views/diaryNoLogIn/top.blade.php
+++ backend/resources/views/diaryNoLogIn/top.blade.php
@@ -15,43 +15,59 @@
             <span>自分で</span><span>作る</span><span>統計データ</span>
         </h2>
     </div>
-    <h2 class="mt-24 mb-12 text-center text-4xl kiwi-maru">流れ</h2>
-    <div>
-        <h3 class="text-center my-4 kiwi-maru text-3xl"><span class="material-icons">history_edu</span>日記を書く</h3>
-        <img src="/img/topDiscribe/kadodeImageMain.jpg" class="object-contain border-2 button-border-main-color">
-    </div>
-    <div>
+    <h2 class="mt-24 mb-12 text-center text-4xl kiwi-maru">--流れ--</h2>
+    <!--区切り-->
+    @component('components.noLogIn.introFrame')
+    @slot('icon')
+    history_edu
+    @endslot
+    @slot('title')
+    日記を書く
+    @endslot
+    <img src="/img/topDiscribe/kadodeImageMain.jpg" class="object-contain border-2 button-border-main-color">
+    @endcomponent
+    <!--区切り-->
+    @component('components.noLogIn.introFrame')
+    @slot('icon')
+    settings
+    @endslot
+    @slot('title')
+    統計の設定を行う
+    @endslot
+    <p class="text-center my-2">一般的な辞書にない単語などを固有表現として登録できます。</p>
+    <img src="/img/topDiscribe/kadodeImageSettingStatistic.jpg"
+        class="object-contain border-2 button-border-main-color">
+    @endcomponent
+    <!--区切り-->
+    @component('components.noLogIn.introFrame')
+    @slot('icon')
+    loop
+    @endslot
+    @slot('title')
+    統計を生成する
+    @endslot
+    <p class="text-center my-2">自然言語処理ライブラリ、GiNZAをベースに用いています。</p>
+    <img src="/img/topDiscribe/kadodeImageGeneratingStatistics.jpg"
+        class="object-contain border-2 button-border-main-color">
+
+
+    <h4 class="relative top-8 my-4 kiwi-maru text-2xl mt-20 bg-main-color inline-block"><span
+            class="material-icons">category</span>処理の流れ</h4>
+    <div class="p-12 border-dashed border-2 border-border-main-color rounded-xl">
         @component('components.statistics.statisticOverallView')
         @endcomponent
     </div>
-    <h2 class="mt-24 mb-12 text-center text-3xl kiwi-maru">ユーザーランク制度</h2>
-    <div>
-        <style>
-            object {
-                margin: 5px auto;
-                width: 900px;
-                height: auto;
-            }
 
-            @media screen and (max-width: 640px) {
-                object {
-                    width: 100%;
-                }
-            }
-        </style>
-        <object type="image/svg+xml" data="/img/userRank/userRankMap.svg" viewBox="0 0 446.16847 309.23255"></object>
-    </div>
-
-    <h2 class="mt-24 mb-12 text-center text-3xl kiwi-maru">できること</h2>
-
-    <div class="flex justify-center imtes-center flex-wrap my-8">
-        <div class="md:w-1/2 w-full md:order-2">
-            <img src="/img/topDiscribe/kadodeImageArchive.jpg" class="object-contain border-2 button-border-main-color">
-        </div>
-        <div class="md:w-1/2 w-full md:order-1 flex items-center justify-center">
-            <p class="text-3xl kiwi-maru">閲覧</p>
-        </div>
-    </div>
+    @endcomponent
+    <!--区切り-->
+    @component('components.noLogIn.introFrame')
+    @slot('icon')
+    menu_book
+    @endslot
+    @slot('title')
+    統計を閲覧する
+    @endslot
+    <p class="text-center my-2">個別、月別、年別、トータルの統計情報をご覧いただけます。</p>
     <div class="flex justify-center imtes-center flex-wrap my-8">
         <div class="md:w-1/2 w-full ">
             <img src="/img/topDiscribe/kadodeImageNlpTotal.jpg"
@@ -78,15 +94,40 @@
             <p class="text-3xl kiwi-maru">統計(個別)</p>
         </div>
     </div>
-    <div class="flex justify-center imtes-center flex-wrap my-8">
-        <div class="md:w-1/2 w-full md:order-2">
-            <img src="/img/topDiscribe/kadodeImageExport.jpg" class="object-contain border-2 button-border-main-color">
-        </div>
-        <div class="md:w-1/2 w-full md:order-1 flex items-center justify-center">
-            <p class="text-3xl kiwi-maru">入出力</p>
-        </div>
+    @endcomponent
+    <!--区切り-->
+    @component('components.noLogIn.introFrame')
+    @slot('icon')
+    settings
+    @endslot
+    @slot('title')
+    他にも……
+    @endslot
+    <h4 class="text-center my-4 kiwi-maru text-2xl"><span class="material-icons">military_tech</span>ユーザーランク制度</h4>
+    <p class="text-center my-2">かどで日記での停泊地に合わせたユーザー名称が付与されます。</p>
+    <div>
+        <style>
+            object {
+                margin: 5px auto;
+                width: 900px;
+                height: auto;
+            }
+
+            @media screen and (max-width: 640px) {
+                object {
+                    width: 100%;
+                }
+            }
+        </style>
+        <object type="image/svg+xml" data="/img/userRank/userRankMap.svg" viewBox="0 0 446.16847 309.23255"></object>
     </div>
-    <p class="text-center mb-4 kiwi-maru">などなど……</p>
+    <h4 class="text-center my-4 kiwi-maru text-2xl mt-20"><span class="material-icons">import_export</span>インポート、エクスポート
+    </h4>
+
+    <img src="/img/topDiscribe/kadodeImageExport.jpg" class="object-contain border-2 button-border-main-color">
+    @endcomponent
+
+
     <!--
      Copyright (c) June 1, 2015 Tuomas Pöyry
      Released under the MIT license
@@ -94,22 +135,22 @@
      -->
     <canvas id="top-animation"></canvas>
     <div class="mb-24 mt-4">
-        <p class="text-center">
-            <a class="border-2 border-border-main-color p-2 text-3xl  kiwi-maru" href="{{url("register")}}">新規登録</a>
-        </p>
         <p class="text-center mt-4">あくまで個人が作った<br class="md:hidden">趣味の遊びサービスです。<br>ご理解の上、ご利用ください。</p>
+        <p class="text-center mt-12">
+            <a class="border-2 border-border-main-color p-2 text-3xl  kiwi-maru rounded-2xl" href="{{url("register")}}">新規登録</a>
+        </p>
     </div>
     <div class="mt-4 mb-12">
-        <h2 class="text-center text-3xl my-4 kiwi-maru">開発者より</h2>
+        <h2 class="text-center text-3xl my-4 kiwi-maru"><span class="material-icons">face</span>開発者より</h2>
         <div class="flex justify-center  md:w-1/2 px-4 mx-auto">
-            <p class="">開発者のうすゆきと申します。<br>日記書くの、おすすめです！</p>
+            <p class="">開発者のうすゆきと申します。<br>Twitter:@usuyuki26<br>日記書くの、おすすめです！</p>
         </div>
     </div>
     <div class="mt-4 mb-24">
-        <h2 class="text-center text-3xl my-4 kiwi-maru">コンセプト</h2>
+        <h2 class="text-center text-3xl my-4 kiwi-maru"><span class="material-icons">straighten</span>コンセプト</h2>
         <div class="flex justify-start  md:w-1/2 px-4 mx-auto">
             <p class="">
-                人は忘れてしまいます。でも記録は残ります。10世紀に書かれた土佐日記も私達へ受け継がれています。かどで日記の「かどで」は土佐日記に登場する最初の章？「門出」より引用しました。一方で、ページデザインはちょっとSFチックな色を選びました。近未来っぽさと古代っぽさを掛け合わせたコンセプトです。
+                人は忘れてしまいます。でも記録は残ります。<br>10世紀に書かれた土佐日記も私達へ受け継がれています。<br>かどで日記の「かどで」は土佐日記に登場する最初の章？「門出」より引用しました。一方で、ページデザインはちょっとSFチックな色を選びました。近未来っぽさと古代っぽさを掛け合わせたコンセプトです。
             </p>
         </div>
     </div>
