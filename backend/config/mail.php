63f1d76aad7039d272f66bc7a8440687ebaf3ac7
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-05-16 00:30:46 +0900

    fix 登録時エラーの修正(テストで気づけた)

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -34,6 +34,15 @@ return [
     */
 
     'mailers' => [
+        'mail' => [
+            'transport' => 'mail',
+            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => env('MAIL_PORT', 587),
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+        ],
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
