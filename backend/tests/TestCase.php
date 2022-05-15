0bae96d4d63d4157759a6bf21a0fe1a599e00f7d
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-05-15 23:51:59 +0900

    wip テストでもシーダーを実行するよう設定(外部キーで引っかかるので)

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -7,4 +7,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * デフォルトのシーダーが各テストの前に実行するかを示す
+     *
+     * @var bool
+     */
+    protected $seed = true;
 }
