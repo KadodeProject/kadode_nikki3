019ef2a192c9eb877af2210fc7e85b736b25d457
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-05-17 14:48:01 +0900

    feat Pythonに投げる処理のusecase化とテスト動作

--- backend/app/UseCases/Statistic/ThrowPythonNLP.php
+++ backend/app/UseCases/Statistic/ThrowPythonNLP.php
@@ -1,24 +1,17 @@
 <?php
 
-namespace App\CustomFunction;
+declare(strict_types=1);
 
+namespace App\UseCases\Statistic;
 
-
-
-
-class throwPython
+class ThrowPythonNLP
 {
     /**
      * 2021/9/21
-     * python側で呼び出さないと、並列処理になってしまい各所でバグの温床になるので、一括実行へ変更
-     *
-     *
-     * @param [type] $user_id
-     * @param boolean $error_check
-     * @param boolean $debug
-     * @return void
+     * python側で呼び出さないと、並列処理になってしまい各所でバグの温床になるので、一括実行へ変更←どういうこと？
+     * $user_idの日記の自然言語処理を走らせる
      */
-    public static function throwNlpToPython($user_id, $error_check = false, $debug = false)
+    public static function invoke(int $user_id, bool $error_check = false, bool $debug = false): array
     {
 
         if ($error_check) {
@@ -28,9 +21,8 @@ class throwPython
             //> /dev/null & すると非同期で実行できる
             $path = "export LANG=ja_JP.UTF-8; " . env('PYTHON_PRO_DIR') . " " . env('PYTHON_FOLDER_DIR') . "pythonUseFromPHP.py" . " " . $user_id . " > /dev/null &";
         }
-        $output = null;
-        // $path = "export LANG=ja_JP.UTF-8;python3 " . env('PYTHON_PRO_DIR') . " " . env('PYTHON_FOLDER_DIR') . "pythonUseFromPHP.py" . " " . $user_id . " > /dev/null &";
-        // dd($path);
+
+        $output = [];
         exec($path, $output); //python実行
 
         if ($debug) {
@@ -41,7 +33,6 @@ class throwPython
             // \Log::debug("command_output:");
             // \Log::debug($return_var);
         }
-
         return $output;
     }
 }
---
+++ backend/tests/Unit/UseCases/Statistic/ThrowPythonNLPTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\UseCases\Statistic;
+
+use App\UseCases\Statistic\ThrowPythonNLP;
+use Tests\TestCase;
+
+class ThrowPythonNLPTest extends TestCase
+{
+    /** @property ThrowPythonNLP */
+    private $throwPythonNLPTest;
+
+    /** @todo これはデータベースにもろ依存している。 */
+    /** @test */
+    public function execコマンドでpythonをエラー無く実行させる()
+    {
+        $this->throwPythonNLPTest = new ThrowPythonNLP();
+
+        /**デバックありで実行(第二引数true) */
+        $response = $this->throwPythonNLPTest->invoke(1, true, false);
+        /**
+         *  デバックありexecコマンドでNLP動くとPython側の標準出力で
+         * 0 => "nlpForPre"
+         * 1 => "DB接続処理開始"
+         * 2 => "2スキップ"
+         * 3 => "3スキップ"
+         * 4 => "4スキップ"
+         * 5 => "5スキップ"
+         * 6 => "6スキップ"
+         * 7 => "7スキップ"
+         * 略
+         * 322 => "DONE
+         * 的なの出てくるので、最後まで実行された時のDONEを見る。
+         *  */
+        $this->assertContains("DONE", $response);
+    }
+}
