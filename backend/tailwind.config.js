93aef29c279f8f615ea017b5ca8311f8fec115b2
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-06-08 11:42:39 +0900

    wip Tailwindにカラー記載を追加

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,26 +1,34 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                kn_b: "#2f3437", //黒
+                kn_w: "#f9fff9", //白
+                kn_1: "#c7ffc4", //紫
+                kn_2: "#ccaa8f", //青
+                kn_3: "#1a1616", //灰色
+                kn_poor: "#1a1616", //赤
+                kn_good: "#1a1616", //緑
+                kn_excellent: "#1a1616", //青                kn_bubble: "#1a1616",//灰色
+                kn_bubble: "#f9fff9", //青                kn_bubble: "#1a1616",//灰色
             },
         },
     },
 
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-        },
-    },
-
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+    ],
 };
