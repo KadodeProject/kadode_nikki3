de2a29e7b8fba14d1349effd243ff1ca074691a7
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-06-08 18:04:58 +0900

    feat ロゴのtitleコメント追加

--- backend/resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ backend/resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,2 +1,2 @@
-
-<img  alt="かどで日記3" style="object-fit:contain;width:auto;height:64px"src="/img/kadode_logo.png">
+<img alt="かどで日記3" style="object-fit:contain;width:auto;height:64px" src="/img/kadode_logo.png"
+    title="エーデルワイスの花言葉は「大切な思い出」です">
--- backend/resources/views/vendor/jetstream/components/authentication-card.blade.php
+++ backend/resources/views/vendor/jetstream/components/authentication-card.blade.php
@@ -1,34 +1,41 @@
 <style>
-    input{
-        background-color:#2F3437!important;
-        border-color: #4B8996!important;
-        color:#f9fff9!important;
+    input {
+        background-color: #2F3437 !important;
+        border-color: #4B8996 !important;
+        color: #f9fff9 !important;
     }
-    input:focus{
-        border-color: #4B8996!important;
+
+    input:focus {
+        border-color: #4B8996 !important;
     }
-    label{
-        color:#f9fff9!important;
+
+    label {
+        color: #f9fff9 !important;
     }
-    a{
-        color:#f9fff9!important;
+
+    a {
+        color: #f9fff9 !important;
     }
-    span{
-        color:#f9fff9!important;
+
+    span {
+        color: #f9fff9 !important;
     }
-    div{
-        color:#f9fff9!important;
+
+    div {
+        color: #f9fff9 !important;
     }
-    button{
-        color:#f9fff9!important;
+
+    button {
+        color: #f9fff9 !important;
     }
 </style>
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 " >
-    <div class="bg-white rounded-full p-4">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 ">
+    <div class="">
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4  shadow-md overflow-hidden sm:rounded-lg" style="background-color:#5a6369;">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4  shadow-md overflow-hidden sm:rounded-lg"
+        style="background-color:#5a6369;">
         {{ $slot }}
     </div>
 </div>
