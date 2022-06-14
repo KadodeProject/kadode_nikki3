71d296daaec53ef3e34c5b6a2fccad12128d4e5a
Naofumi <63891531+Usuyuki@users.noreply.github.com>
2022-06-14 09:19:35 +0900

    fix typo

--- backend/resources/views/components/buttons/editDiaryButton.blade.php
+++ backend/resources/views/components/buttons/editDiaryButton.blade.php
@@ -1,4 +1,4 @@
 <div class="submit-button">
     <a class="flex items-center justify-center flex-row text-kn_w" href="{{url('/edit')}}/{{$edit_uuid}}"><span
-            class="material-icons">sticky_note_2</span><span class="kiwi-maru">閲覧と閲覧</span></a>
+            class="material-icons">sticky_note_2</span><span class="kiwi-maru">閲覧と編集</span></a>
 </div>
