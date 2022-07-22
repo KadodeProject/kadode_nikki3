9d5c3364d1601c8a5bef27be67545174c05c8020
Usuyuki <fugufugu0206@gmail.com>
2022-07-22 16:41:34 +0900

    wip CIの修正など

--- backend/tests/Feature/Diary/CreateDiaryTest.php
+++ backend/tests/Feature/Diary/CreateDiaryTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature\Diary;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Jetstream\Features;
-use Laravel\Jetstream\Http\Livewire\ApiTokenManager;
 use Livewire\Livewire;
 use Tests\TestCase;
 
@@ -16,19 +15,5 @@ class CreateDiaryTest extends TestCase
     /** @test */
     public function 日記を作成()
     {
-        Livewire::test(ApiTokenManager::class)
-            ->set(['createApiTokenForm' => [
-                'name' => 'Test Token',
-                'permissions' => [
-                    'read',
-                    'update',
-                ],
-            ]])
-            ->call('createApiToken');
-
-        $this->assertCount(1, $user->fresh()->tokens);
-        $this->assertEquals('Test Token', $user->fresh()->tokens->first()->name);
-        $this->assertTrue($user->fresh()->tokens->first()->can('read'));
-        $this->assertFalse($user->fresh()->tokens->first()->can('delete'));
     }
 }
