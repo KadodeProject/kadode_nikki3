03ead33ef234191d518671971041dcfa6687c3ab
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-07-26 22:44:01 +0900

    fix 文字列だけの配列になるよう変更

--- backend/app/Console/Commands/GCSCommand.php
+++ backend/app/Console/Commands/GCSCommand.php
@@ -57,6 +57,19 @@ class GCSCommand extends Command
         ));
         closedir($dirH);
 
+        /**
+         * $fileListは
+         * ^ array:5 [
+         *     0 => "2022-07-26-22-22-06.zip"
+         *     1 => "2022-07-26-22-37-15.zip"
+         *     2 => ".."
+         *     3 => "."
+         *     4 => false
+         *     ]
+         * なので、後ろ3つを除く
+         */
+        array_splice($fileList, -3);
+
         //最新ファイルの探索
         $timer = date_create_immutable_from_format("Y-m-d-H-i-s", "2021-10-20-07-16-00");
         $recentFile = "";
