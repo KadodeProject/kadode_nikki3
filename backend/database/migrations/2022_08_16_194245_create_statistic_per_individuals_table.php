524f7995d8a74e9183fa17656f915058f859e991
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-08-20 08:58:02 +0900

    feat python側の新テーブル動作修正完了

--- backend/database/migrations/2022_08_16_194245_create_statistic_per_individuals_table.php
+++ backend/database/migrations/2022_08_16_194245_create_statistic_per_individuals_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger("diary_id")->comment("日記のid");
 <<<<<<< HEAD
+<<<<<<< HEAD
             $table->integer("statistic_progress")->nullable()->comment("生成状況(生成まで時間かかるので)");
 
             //nlp表示系
@@ -32,18 +33,20 @@ return new class extends Migration
             $table->json("token")->nullable()->comment("形態素分析された中身を格納 品詞(POS)、原形(lemma)などが存在");
             $table->json("affiliation")->nullable()->comment("固有表現抽出");
             $table->unsignedBigInteger("char_length")->nullable()->comment("文字数");
+=======
+            $table->integer("statistic_progress")->nullable()->comment("生成状況(生成まで時間かかるので)");
+>>>>>>> a502197 (feat python側の新テーブル動作修正完了)
 
             //nlp表示系
-            $table->json("meta_info")->nullable()->comment("事実上予備領域");
-            $table->json("similar_sentences")->nullable()->comment("似ている日記の日記ID(5)");
             $table->double("emotions")->nullable()->comment("感情数値化");
-            $table->double("flavor")->nullable()->comment("ユーザーの日記らしさ");
             $table->string("classification")->nullable()->comment("推定分類");
             $table->json("important_words")->nullable()->comment("重要そうな言葉(top3)");
-            $table->json("cause_effect_sentences")->nullable()->comment("原因と結果のjson");
             $table->json("special_people")->nullable()->comment("登場人物");
+<<<<<<< HEAD
             $table->dateTime("updated_statistic_at")->nullable()->comment("統計更新日時");
 >>>>>>> b88c758 (wip ALTERテーブルとartisan migrateで別個に変更しても壊れないこと確認)
+=======
+>>>>>>> a502197 (feat python側の新テーブル動作修正完了)
 
             $table->timestamps();
 
