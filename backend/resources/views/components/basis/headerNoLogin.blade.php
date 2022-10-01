cf2f0b50c774c2f1cbe3322499ea98e513472577
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-10-01 14:43:46 +0900

    fix トップページのヘッダーのタイトルが微妙にずれている

--- backend/resources/views/components/basis/headerNoLogin.blade.php
+++ backend/resources/views/components/basis/headerNoLogin.blade.php
@@ -1,16 +1,15 @@
 <header class="px-4 relative w-screen flex justify-center sm:justify-between flex-wrap "
     style="box-shadow: 0px 8px 8px -5px rgba(0,0,0,0.5);">
-    <a href="{{route('ShowHome')}}"><img class="logo-header" src="/img/kadode_logo.svg"
+    <a class="md:flex-1 flex-none" href="{{route('ShowHome')}}"><img class="logo-header" src="/img/kadode_logo.svg"
             title="エーデルワイスの花言葉は「大切な思い出」です"></a>
 
     </div>
-    <div class="flex items-center">
+    <div class="fmd:flex-1 flex-none flex items-center">
 
         <h2 class="text-3xl kiwi-maru"><a href="{{route('ShowTop')}}">かどで日記3</a></h2>
     </div>
-    <div class="p-2 sm:w-auto sm:mb-0 mb-4 md:w-auto w-full">
-        @if (Route::has('login'))
-        <div class="flex justify-center">
+    <div class="md:flex-1 flex-none p-2 sm:w-auto sm:mb-0 mb-4 md:w-auto w-full">
+        <div class="flex justify-center md:justify-end">
             @auth
             <a href="{{ route('ShowHome') }}" class="kiwi-maru kadode-normal-button">ホームへ</a>
             @else
@@ -21,11 +20,5 @@
             @endif
             @endauth
         </div>
-        @endif
-
-
-
-
     </div>
-
 </header>
