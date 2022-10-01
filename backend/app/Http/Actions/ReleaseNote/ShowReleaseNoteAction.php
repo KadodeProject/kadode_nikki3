3c90d42484e3bee6f3c246fdf7c72d88900def13
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-10-02 01:01:08 +0900

    wip SQLが極端に多いところにCOMMENT付与

--- backend/app/Http/Actions/ReleaseNote/ShowReleaseNoteAction.php
+++ backend/app/Http/Actions/ReleaseNote/ShowReleaseNoteAction.php
@@ -11,8 +11,9 @@ use Illuminate\Contracts\View\View;
 
 final class ShowReleaseNoteAction extends Controller
 {
-    public function __invoke():View|Factory
+    public function __invoke(): View|Factory
     {
+        /** @todo ジャンル名の呼び出しがwith使ってるけど、内部的にはループでwith呼び出されている雰囲気なので修正した */
         $releasenotes = Releasenote::with('Releasenote_genre:id,name')->orderBy('date', 'desc')->get(['title', 'date', 'genre_id', 'description']);
         return view('diaryNoLogIn/releasenote', ['releasenotes' => $releasenotes,]);
     }
