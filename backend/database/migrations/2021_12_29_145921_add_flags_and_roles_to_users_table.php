cca0f3697dbaf4aa2259e6ce398aa5534e1698d7
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-10-16 21:41:06 +0900

    wip テーブル作成

--- backend/database/migrations/2021_12_29_145921_add_flags_and_roles_to_users_table.php
+++ backend/database/migrations/2021_12_29_145921_add_flags_and_roles_to_users_table.php
@@ -14,9 +14,6 @@ class AddFlagsAndRolesToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean("is_showed_update_user_rank")->nullable()->comment("ユーザーランクの更新通知フラグ")->default(0);
-            $table->boolean("is_showed_update_system_info")->nullable()->comment("アップデート情報更新通知フラグ")->default(0);
-            $table->boolean("is_showed_service_info")->nullable()->comment("お知らせ通知フラグ")->default(0);
             $table->unsignedBigInteger("user_rank_id")->nullable()->comment("ユーザーランク")->default(1);
             $table->unsignedBigInteger("user_role_id")->nullable()->comment("ユーザーロール(一般、管理者etc)")->default(1);
             $table->unsignedBigInteger("appearance_id")->nullable()->comment("ページの見た目")->default(1);
@@ -26,18 +23,18 @@ class AddFlagsAndRolesToUsersTable extends Migration
 
             //他テーブルとの関連付け
             $table->foreign('user_rank_id')
-            ->references('id')
-            ->on('user_ranks');
+                ->references('id')
+                ->on('user_ranks');
 
             //他テーブルとの関連付け
             $table->foreign('user_role_id')
-            ->references('id')
-            ->on('user_roles');
+                ->references('id')
+                ->on('user_roles');
 
             //他テーブルとの関連付け
             $table->foreign('appearance_id')
-            ->references('id')
-            ->on('appearances');
+                ->references('id')
+                ->on('appearances');
         });
     }
 
---
+++ backend/database/migrations/2022_10_16_210100_create_user_notifications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_notifications', function (Blueprint $table) {
+            $table->id();
+            $table->boolean("is_showed_update_user_rank")->nullable()->comment("ユーザーランクの更新通知フラグ")->default(0);
+            $table->boolean("is_showed_update_system_info")->nullable()->comment("アップデート情報更新通知フラグ")->default(0);
+            $table->boolean("is_showed_service_info")->nullable()->comment("お知らせ通知フラグ")->default(0);
+            $table->timestamps();
+            //他テーブルとの関連付け
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade'); //cascadeでユーザー消えたらipも消せる
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_notifications');
+    }
+};
