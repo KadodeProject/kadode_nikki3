c87c88f1e2b199d13f57819ffd1b342700ac5155
Usuyuki <fugufugu0206@gmail.com>
2022-10-20 18:02:30 +0900

    fix cs-fxierかけてなかった&&phpunitのキャッシュも乗せる

--- backend/app/Http/Actions/ShowAdminPhpInfo.php
+++ backend/app/Http/Actions/ShowAdminPhpInfo.php
@@ -5,14 +5,10 @@ declare(strict_types=1);
 namespace App\Http\Actions;
 
 use App\Http\Controllers\Controller;
-use App\Models\UserRank;
-use App\Models\UserRole;
-use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\View\View;
 
 final class ShowAdminPhpInfo extends Controller
 {
-    public function __invoke()
+    public function __invoke(): void
     {
         phpinfo();
     }
