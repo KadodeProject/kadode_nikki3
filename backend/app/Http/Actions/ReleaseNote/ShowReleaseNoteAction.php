8e20293af1326fa2e87a23c3ba50f998330ffa11
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-10-27 09:41:15 +0900

    docs 文章周り追記

--- backend/app/Http/Actions/ReleaseNote/ShowReleaseNoteAction.php
+++ backend/app/Http/Actions/ReleaseNote/ShowReleaseNoteAction.php
@@ -13,7 +13,6 @@ final class ShowReleaseNoteAction extends Controller
 {
     public function __invoke(): View|Factory
     {
-        /** @todo ジャンル名の呼び出しがwith使ってるけど、内部的にはループでwith呼び出されている雰囲気なので修正したい */
         $releasenotes = Releasenote::with('releasenoteGenre:id,name')->orderBy('date', 'desc')->get(['title', 'date', 'genre_id', 'description']);
 
         return view('diaryNoLogIn/releasenote', ['releasenotes' => $releasenotes]);
---
+++ docs/DEV/05_production_infra/03_uploadSize.md
@@ -0,0 +1,10 @@
+# アップロード上限について
+
+従来：php.ini に記述
+現在：Nginx の conf に記述
+
+```
+413 Request Entity Too Large laravel
+```
+
+になった時は確認を。
