d52b7b8462663e09a5fffaea0be24d038eefe540
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-11-23 19:56:54 +0900

    feat 直近1分取得APIを追加

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -26,6 +26,7 @@ Route::get('OperationCoreTransitionPerHours/relative/week', \App\Http\ApiActions
 Route::get('OperationCoreTransitionPerHours/relative/month', \App\Http\ApiActions\OperationCoreTransition\GetOperationCoreTransitionLatestMonth::class)->name('GetOperationCoreTransitionLatestMonth');
 
 // サーバーのリソース取得API
+Route::get('MachineResource/relative/1min', \App\Http\ApiActions\MachineResource\GetMachineResourceLatest1Minutes::class)->name('GetMachineResourceLatest1Minutes');
 Route::get('MachineResource/relative/30min', \App\Http\ApiActions\MachineResource\GetMachineResourceLatest30Minutes::class)->name('GetMachineResourceLatest30Minutes');
 Route::get('MachineResource/relative/day', \App\Http\ApiActions\MachineResource\GetMachineResourceLatestDay::class)->name('GetMachineResourceLatestDay');
 Route::get('MachineResource/relative/week', \App\Http\ApiActions\MachineResource\GetMachineResourceLatestWeek::class)->name('GetMachineResourceLatestWeek');
