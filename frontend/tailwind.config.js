c496b10740bfa9880c8a4c3a3e659e5d2b20cf6a
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-11-23 16:06:39 +0900

    feat next13対応

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
