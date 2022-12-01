2f7cd706a5d079d8f9cfd3a8b36e32897ce06827
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-12-01 15:34:20 +0900

    fix websocket疎通

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
     return (
         <div>
-            <h1 className='text-3xl'>かどで日記</h1>
+            <h1 className='text-3xl'>かどで日記!!!!</h1>
         </div>
     );
 };
