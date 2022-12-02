6f0166ca088b6a7ed15be0f3275c2c5d24a37f6f
Usuyuki <63891531+Usuyuki@users.noreply.github.com>
2022-12-03 00:15:51 +0900

    fix @next/fontが13.0.6時点では日本語うまく動作しないので既存の方式で読み込む

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,19 +1,20 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-//フォント読み込み
-import { Kiwi_Maru, Zen_Kurenaido } from '@next/font/google';
+//フォント読み込み 13.0.6時点では日本語フォントが非対応であったため断念、動作はするのでコメントアウトしておく
+/** @todo @next/fontが日本語対応したらコメントアウト外す */
+// import { Kiwi_Maru, Zen_Kurenaido } from '@next/font/google';
 
-const KiwiMaruRegular = Kiwi_Maru({
-    weight: '400',
-    subsets: ['japanese'],
-    variable: '--font-kiwi-maru',
-});
-const ZenKurenaidoRegular = Zen_Kurenaido({
-    weight: '400',
-    subsets: ['japanese'],
-    variable: '--font-zen-kurenaido',
-});
+// const KiwiMaruRegular = Kiwi_Maru({
+//     weight: '400',
+//     subsets: ['japanese'],
+//     variable: '--font-kiwi-maru',
+// });
+// const ZenKurenaidoRegular = Zen_Kurenaido({
+//     weight: '400',
+//     subsets: ['japanese'],
+//     variable: '--font-zen-kurenaido',
+// });
 
 // ダークモード対応
 import { ThemeProvider } from 'next-themes';
@@ -27,15 +28,13 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <ThemeProvider attribute="class" defaultTheme="dark">
-                <div className={`${KiwiMaruRegular.variable} ${ZenKurenaidoRegular.variable}`}>
-                    <Component {...pageProps} />
-                </div>
-            </ThemeProvider>
-        </>
-    );
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark">
+      {/* <div className={`${KiwiMaruRegular.variable} ${ZenKurenaidoRegular.variable}`}> */}
+      <Component {...pageProps} />
+      {/* </div> */}
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
