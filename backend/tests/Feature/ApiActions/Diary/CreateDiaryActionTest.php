ee9062c7d9730c4e6e463b63040351793cd0b7b8
usuyuki <63891531+Usuyuki@users.noreply.github.com>
2023-04-17 23:38:19 +0900

    feat createDiaryActionのテストを整備

---
+++ backend/tests/Feature/ApiActions/Diary/CreateDiaryActionTest.php
@@ -0,0 +1,135 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\ApiActions;
+
+use App\Models\Diary;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class CreateDiaryActionTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test正しい値と認証で日記が作れる(): void
+    {
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        $user = User::factory()->create([
+            'email' => 'testGetHomeAction@example.com',
+            'password' => 'password',
+        ]);
+
+        $this->actingAs($user)
+            ->postJson('/api/diary/create',
+            [
+                'date' => '1999-01-01',
+                'title' => 'test',
+                'content' => 'test',
+            ])
+            ->assertStatus(200);
+
+        $this->assertDatabaseHas('diaries', [
+            'date' => '1999-01-01',
+            'title' => 'test',
+            'content' => 'test',
+        ]);
+    }
+
+    public function test認証していないと401エラー():void
+    {
+        $this->postJson('/api/diary/create',
+            [
+                'date' => '1999-01-01',
+                'title' => 'test',
+                'content' => 'test',
+            ])
+            ->assertStatus(401);
+    }
+
+    //タイトルは空でも良いので
+    public function test内容が空だとエラー():void
+    {
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        $user = User::factory()->create([
+            'email' => 'testGetHomeAction@example.com',
+            'password' => 'password',
+        ]);
+
+        $this->actingAs($user)
+            ->postJson('/api/diary/create',
+            [
+                'date' => '1999-01-01',
+                'title' => '',
+                'content' => '',
+            ])
+            ->assertStatus(422)
+            ->assertJson([
+                "message"=> "本文は、必ず指定してください。",
+                "errors"=> [
+                    "content"=> [
+                    "本文は、必ず指定してください。"
+                    ]
+                ]
+            ]);
+        }
+
+    public function test日付が空だとエラー():void
+    {
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        $user = User::factory()->create([
+            'email' => 'testGetHomeAction@example.com',
+            'password' => 'password',
+        ]);
+
+        $this->actingAs($user)
+            ->postJson('/api/diary/create',
+            [
+                'date' => '',
+                'title' => '',
+                'content' => 'hoge',
+            ])
+            ->assertStatus(422)
+            ->assertJson([
+                "message"=> "日付は、必ず指定してください。",
+            ]);
+        }
+
+    public function test既に存在する日付だとエラー():void
+    {
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        $user = User::factory()->create([
+            'email' => 'testGetHomeAction@example.com',
+            'password' => 'password',
+        ]);
+
+
+        Diary::create(
+            [
+                'date' => '1999-01-01',
+                'title' => 'test',
+                'content' => 'test',
+                'user_id' => $user->id,
+                'uuid' => Str::uuid(),
+            ]
+            );
+
+        $this->actingAs($user)
+            ->postJson('/api/diary/create',
+            [
+                'date' => '1999-01-01',
+                'title' => '',
+                'content' => 'hoge',
+            ])
+            ->assertStatus(422)
+            ->assertJson([
+                "message"=> "既に存在する日付の日記は作成できません",
+            ]);
+        }
+}
