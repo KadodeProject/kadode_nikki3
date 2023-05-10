05665e7b1ac934aa7394c0a545b6f325044f285f
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-05-10 17:18:21 +0900

    wip いったんコミット

---
+++ backend/app/Http/ApiActions/SocialLogin/GetProviderOAuthURLAction.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Actions\OAuth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Responder\OAuthResponder;
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+
+final class GetProviderOAuthURLAction extends Controller
+{
+  // private $domain;
+  // private $responder;
+
+  // public function __construct()
+  // {
+
+  // }
+
+  /**
+   * Undocumented function
+   *
+   * @param string $provider 認証プロバイダーとなるサービス名
+   */
+  public function __invoke(string $provider): \Illuminate\Http\JsonResponse
+  {
+    assert(in_array($provider, ['google', 'github', 'line']));
+    $redirectUrl = Socialite::driver($provider)->redirect()->getTargetUrl();
+
+    return response()->json([
+      'redirect_url' => $redirectUrl,
+    ]);
+  }
+}
---
+++ backend/app/Http/ApiActions/SocialLogin/HandleProviderCallbackAction.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Actions\OAuth;
+
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+final class HandleProviderCallbackAction extends Controller
+{
+  /**
+   * ソーシャルログイン処理
+   * @return App\User
+   */
+  public function __invoke()
+  {
+    $googleUser = Socialite::driver('google')->ser();
+
+    $user  = User::where('google_id', $googleUser->id)->first();
+    $token = Str::random(80);
+
+    if ($user) {
+      $user->update([
+        'name' => $googleUser->name,
+        'email' => $googleUser->email,
+        'email_verified_at' => now(),
+        'profile_photo_path' => $googleUser->avatar,
+        'login_provider' => 1,
+        'google_token' => $googleUser->token,
+        'google_refresh_token' => $googleUser->refreshToken,
+        'api_token' => hash('sha256', $token),
+      ]);
+    } else {
+      $user = User::create([
+        'name' => $googleUser->name,
+        'email' => $googleUser->email,
+        'email_verified_at' => now(),
+        'profile_photo_path' => $googleUser->avatar,
+        'login_provider' => 1,
+        'google_id' => $googleUser->id,
+        'google_token' => $googleUser->token,
+        'google_refresh_token' => $googleUser->refreshToken,
+        'api_token' => hash('sha256', $token),
+      ]);
+    }
+
+    Auth::login($user);
+
+    $cookie = cookie('api_token', $token, '10000000', null, null, null, false);
+    return redirect(env('FRONTEND_URL') . '/home')->cookie($cookie);
+  }
+}
