8fd415306fdb06ae29a6624a0f891f46a2403954
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-08-06 22:13:45 +0900

    wip

--- frontend/src/lib/utils/client/backendAuthenticatedApiClient.ts
+++ frontend/src/lib/utils/client/backendAuthenticatedApiClient.ts
@@ -1,6 +1,7 @@
 import { apiUrlResolver } from '$src/lib/utils/apiUrlResolver';
 import api from '$apiSchema/$api';
 import aspida from '@aspida/node-fetch';
+import fetch from 'node-fetch';
 import type { RequestEvent } from '@sveltejs/kit';
 
 import { env } from '$env/dynamic/public';
