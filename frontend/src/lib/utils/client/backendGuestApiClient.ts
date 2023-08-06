a3e9caa64656278123ce4d4ab0d56a5e4aad65c2
usuyuki <63891531+usuyuki@users.noreply.github.com>
2023-08-06 22:18:45 +0900

    fix 依存関係の修正

--- frontend/src/lib/utils/client/backendGuestApiClient.ts
+++ frontend/src/lib/utils/client/backendGuestApiClient.ts
@@ -1,7 +1,7 @@
 import { apiUrlResolver } from '$src/lib/utils/apiUrlResolver';
 import api from '$apiSchema/$api';
 import aspida from '@aspida/node-fetch';
-
+import fetch from 'node-fetch';
 export const gApiClient = () => {
 	const aspidaFetchConfig = {
 		baseURL: apiUrlResolver(),
